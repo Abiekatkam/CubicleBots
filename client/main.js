c9002cb56b53d5c863d4be08d3bfe5873518ec5b
Abiekatkam <abhishekkatkam30@gmail.com>
2022-12-24 10:27:43 +0530

    Update changes

--- client/main.js
+++ client/main.js
@@ -72,7 +72,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch("https://cubiclebots.onrender.com", {
+  const response = await fetch("http://localhost:5000", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
